8cbb9d2ff01e92df225d7d33048cde05233faefb
onwl007 <onwl007@126.com>
2020-03-28 21:45:16 +0800

    feat: add tag store

--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -2,9 +2,11 @@
 import * as app from './app'
 import * as auth from './auth'
 import * as category from './category'
+import * as tag from './tag'
 
 export default {
   app: { namespaced: true, ...app },
   auth: { namespaced: true, ...auth },
   category: { namespaced: true, ...category },
+  tag: { namespaced: true, ...tag },
 }
